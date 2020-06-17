fc043f800ff188259d0e3143abb3d5f4032756ce
Vincent Le Badezet <vincent.l@lajavaness.com>
2020-06-17 10:37:15 +0200

    test: Refactor navigator.permissions and navigator.mediaDevices mocking

--- src/__tests__/getUserMediaStream.test.js
+++ src/__tests__/getUserMediaStream.test.js
@@ -1,66 +1,129 @@
 import getUserMediaStream from '../getUserMediaStream'
 
-import NavigatorPermissionsMock from './NavigatorPermissionsMock'
-import NavigatorMediaDevicesMock from './NavigatorMediaDevicesMock'
+describe('getUserMediaStream', () => {
+	describe('navigator.permissions is not implemented', () => {
+		beforeAll(() => {
+			global.navigator.permissions = undefined
+		})
 
-describe('userMediaUtils', () => {
-	describe('getUserMediaStream', () => {
-		it('rejects promise since navigator.permissions is not implemented', async () => {
+		it('rejects promise', async () => {
 			try {
 				await getUserMediaStream()
 			} catch (error) {
 				expect(error).toEqual(new Error('PERMISSIONS_NOT_SUPPORTED'))
 			}
 		})
+	})
 
-		it('rejects promise since user has previously denied permissions', async () => {
-			NavigatorPermissionsMock.mock('denied')
-			try {
-				await getUserMediaStream()
-			} catch (error) {
-				expect(error).toEqual(new Error('DENIED_BY_USER'))
-			}
+	describe('navigator.permissions is implemented', () => {
+		const mockPermissionsQuery = jest.fn()
+
+		beforeAll(() => {
+			global.PermissionStatus = jest.fn(() => ({
+				state: 'granted',
+				addEventListener: jest.fn(),
+			}))
+			global.Permissions = jest.fn(() => ({
+				query: mockPermissionsQuery,
+			}))
+			global.navigator.permissions = new Permissions()
 		})
 
-		it('rejects promise since navigator.mediaDevices is not implemented', async () => {
-			NavigatorPermissionsMock.mock('prompt')
-			try {
-				await getUserMediaStream()
-			} catch (error) {
-				expect(error).toEqual(new Error('MEDIA_DEVICES_NOT_SUPPORTED'))
-			}
+		afterAll(() => {
+			global.PermissionStatus.mockReset()
+			global.Permissions.mockReset()
 		})
 
-		it('resolves promise with stream since user has previously granted permission', async () => {
-			NavigatorPermissionsMock.mock('granted')
-			NavigatorMediaDevicesMock.mock('foo')
-			try {
-				const stream = await getUserMediaStream()
-				expect(stream).toBe('foo')
-			} catch (error) {
-				throw error
-			}
+		describe('navigator.mediaDevices is not implemented', () => {
+			beforeAll(() => {
+				global.navigator.mediaDevices = undefined
+			})
+
+			it('rejects promise', async () => {
+				const status = new PermissionStatus()
+				status.state = 'prompt'
+				mockPermissionsQuery.mockResolvedValue(status)
+				try {
+					await getUserMediaStream()
+				} catch (error) {
+					expect(error).toEqual(new Error('MEDIA_DEVICES_NOT_SUPPORTED'))
+				}
+			})
 		})
 
-		it('rejects promise since user has been prompted and has denied permissions', async () => {
-			NavigatorPermissionsMock.mock('prompt', 'denied')
-			NavigatorMediaDevicesMock.mock('foo')
-			try {
-				await getUserMediaStream()
-			} catch (error) {
-				expect(error).toEqual(new Error('DENIED_BY_USER'))
-			}
-		})
+		describe('navigator.mediaDevices is implemented', () => {
+			beforeAll(() => {
+				global.MediaDevices = jest.fn(() => ({
+					getUserMedia: jest.fn().mockResolvedValue('foo'),
+				}))
+				global.navigator.mediaDevices = new MediaDevices()
+			})
 
-		it('resolves promise with stream since user has been prompted and has granted permissions', async () => {
-			NavigatorPermissionsMock.mock('prompt', 'granted')
-			NavigatorMediaDevicesMock.mock('foo')
-			try {
-				const stream = await getUserMediaStream()
-				expect(stream).toBe('foo')
-			} catch (error) {
-				throw error
-			}
+			afterAll(() => {
+				global.MediaDevices.mockReset()
+			})
+
+			it('rejects promise since user has previously denied permissions', async () => {
+				const status = new PermissionStatus()
+				status.state = 'denied'
+				mockPermissionsQuery.mockResolvedValue(status)
+				try {
+					await getUserMediaStream()
+				} catch (error) {
+					expect(error).toEqual(new Error('DENIED_BY_USER'))
+				}
+			})
+
+			it('resolves promise with stream since user has previously granted permission', async () => {
+				const status = new PermissionStatus()
+				status.state = 'granted'
+				mockPermissionsQuery.mockResolvedValue(status)
+				try {
+					const stream = await getUserMediaStream()
+					expect(stream).toBe('foo')
+				} catch (error) {
+					throw error
+				}
+			})
+
+			it('rejects promise since user has been prompted and has denied permissions', async () => {
+				const status = new PermissionStatus()
+				status.state = 'prompt'
+				status.addEventListener = jest.fn((e, cb) => {
+					const event = {
+						target: {
+							state: 'denied',
+						},
+					}
+					cb(event)
+				})
+				mockPermissionsQuery.mockResolvedValue(status)
+				try {
+					await getUserMediaStream()
+				} catch (error) {
+					expect(error).toEqual(new Error('DENIED_BY_USER'))
+				}
+			})
+
+			it('resolves promise with stream since user has been prompted and has granted permissions', async () => {
+				const status = new PermissionStatus()
+				status.state = 'prompt'
+				status.addEventListener = jest.fn((e, cb) => {
+					const event = {
+						target: {
+							state: 'granted',
+						},
+					}
+					cb(event)
+				})
+				mockPermissionsQuery.mockResolvedValue(status)
+				try {
+					const stream = await getUserMediaStream()
+					expect(stream).toBe('foo')
+				} catch (error) {
+					throw error
+				}
+			})
 		})
 	})
 })
